e7c025abe8dca6cc421e4c4347a9f825e455ef6b
matheus <matheusgomes1203@hotmail.com>
2020-12-08 20:03:15 -0300

    Adicionando arquivos de configuraçao das ferramentar do projeto

---
+++ settings.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+const settings = {
+  // src folder
+  src: {
+    source: __dirname + '/src/', // source folder
+    images: __dirname + '/src/img/', // images
+    js: __dirname + '/src/js/', // javascripts
+    sass: __dirname + '/src/sass/', // sass
+  },
+
+  // dist folder
+  dist: {
+    css: __dirname + '/dist/css/', // stylesheets
+    js: __dirname + '/dist/js/', // javascripts
+    images: __dirname + '/dist/img/', // images
+  },
+};
+
+module.exports = settings;
